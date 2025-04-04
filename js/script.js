bd20167abb07b220d2aa6e547819491f3dbdd77c
RedEmber-hub <redember38@gmail.com>
2025-04-04 15:46:46 +0300

    Методы и свойства переменных

--- js/script.js
+++ js/script.js
@@ -11,3 +11,12 @@ adaptive = true;
 alert('Привет, рада тебя видеть!');
 
 console.log('Как дела?');
+console.log(typeof title, typeof fullPrice, typeof adaptive);
+console.log(screens.length);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
+console.log(screens.toLowerCase().split(" "));
+console.log(fullPrice * (rollback / 100));
+
+
+
