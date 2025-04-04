326f2f88d8ed4f0120b0ffc3e70f0b905de81479
RedEmber-hub <redember38@gmail.com>
2025-04-04 15:31:18 +0300

    Присвоены значения переменным

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 let title, screens, screenPrice, rollback, fullPrice, adaptive;
 
+title = 'Пушистики';
+screens = 'Простые, Сложные, Интерактивные';
+screenPrice = 50;
+rollback = 10;
+fullPrice = 500;
+adaptive = true;
+
+
 alert('Привет, рада тебя видеть!');
 
 console.log('Как дела?');
