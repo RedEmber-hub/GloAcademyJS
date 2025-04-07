8d215e0e0fc74a02191d8ca7d53d6f0983811815
RedEmber-hub <redember38@gmail.com>
2025-04-07 23:15:57 +0300

    Выполненное задание 4 урока

--- js/script.js
+++ js/script.js
@@ -1,21 +1,30 @@
 alert('Привет, рада тебя видеть!');
 
-let title = prompt('Как называется ваш проект?');
-let screens = prompt('Какие типы экранов нужно разработать?');
+const title = prompt('Как называется ваш проект?');
+const screens = prompt('Какие типы экранов нужно разработать?');
 let screenPrice = parseFloat(prompt('Сколько будет стоить данная работа?'));
-let adaptive = confirm('Нужен ли адаптив на сайте?');
-let service1 = prompt('Какой дополнительный тип услуги нужен?');
+const adaptive = confirm('Нужен ли адаптив на сайте?');
+const service1 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice1 = parseFloat(prompt('Сколько это будет стоить?'));
-let service2 = prompt('Какой дополнительный тип услуги нужен?');
+const service2 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice2 = parseFloat(prompt('Сколько это будет стоить?'));
 
-let fullPrice = screenPrice + servicePrice1 + servicePrice2; // итоговая стоимость работы
-
 let rollback = 10;
-let servicePercentPrice = (fullPrice - (fullPrice * (rollback / 100))); //цена со скидкой
-servicePercentPrice = Math.ceil(servicePercentPrice); //округляем цену в большую сторону
 
-console.log('Как дела?');
+// ф-ция для расчета стоимости всех дополнительных услуг
+const getAllServicePrices = () => {
+    return servicePrice1 + servicePrice2;
+}
+
+//ф-ция возвращает сумму стоимости верстки и стоимости дополнительных услуг
+function getFullPrice(screenPrice, allServicePrices) {
+    return screenPrice + allServicePrices;
+}
+
+//Функция возвращает title меняя его таким образом: первый символ с большой буквы, остальные с маленькой"
+const getTitle = () => {
+    return title.trim().charAt(0).toUpperCase() + title.trim().slice(1).toLowerCase();
+}
 
 const getRollbackMessage = (price) => {
     if (price > 30000) {
@@ -28,15 +37,30 @@ const getRollbackMessage = (price) => {
         return 'Что то пошло не так';
     }
 }
-console.log(getRollbackMessage(fullPrice));
 
-console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
-console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
-console.log(`Процент отката посреднику за работу ${fullPrice * (rollback / 100)}`);
-console.log(`Стоимость за вычетом отката посредника ${servicePercentPrice} рублей`);
-console.log(`Тип данных title: ${typeof title}, тип данных fullPrice: ${typeof fullPrice}, тип данных adaptive: ${typeof adaptive}`);
-console.log(`Длина строки переменной screens: ${screens.length}`);
+//Функция возвращает итоговую стоимость за вычетом процента отката/цена со скидкой
+const getServicePercentPrices = (fullPrice, rollback) => {
+    return (fullPrice - (fullPrice * (rollback / 100)))
+}
+
+const showTypeOf = function (variable) {
+    console.log(variable, typeof variable);
+}
+
+showTypeOf(title);
+showTypeOf(screenPrice);
+showTypeOf(adaptive);
+
+const allServicePrices = getAllServicePrices(); // вычисляем все дополнительные услуги
+
+let fullPrice = getFullPrice(screenPrice, allServicePrices) // итоговая стоимость работы
+
+let servicePercentPrice = getServicePercentPrices(fullPrice, rollback); // цена со скидкой
+servicePercentPrice = Math.ceil(servicePercentPrice);  // округляем цену в большую сторону
+
 console.log(`Массив строки: ${screens.toLowerCase().split(" ")}`);
+console.log(getRollbackMessage(fullPrice));
+console.log(`Стоимость за вычетом отката посреднику ${servicePercentPrice} рублей`);
 
 
 
